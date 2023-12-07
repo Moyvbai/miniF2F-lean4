f6a698ebc572bc86a475921dc9e0458028b8a0e6
Your Name <you@example.com>
2023-12-07 23:49:57 +0800

    update

--- Main.lean
+++ Main.lean
@@ -1,4 +1,12 @@
-import «MiniF2FLean4»
+-- import «MiniF2FLean4»
 
-def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
+-- def main : IO Unit :=
+--   IO.println s!"Hello, {hello}!"
+
+open Nat (add_assoc add_comm)
+
+theorem hello_world (a b c : Nat)
+  : a + b + c = a + c + b := by
+  rw [add_assoc, add_comm b, ←add_assoc]
+
+theorem foo (a : Nat) : a + 1 = Nat.succ a := by rfl
